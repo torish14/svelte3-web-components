60dc61ef8972318b998806478e86d4f1744097f5
torish14 <nanamin.104@icloud.com>
2022-01-08 16:41:44 +0900

    ✨ feat: クリックしたらセルの状態を変えられるようにするため､lifegame.js から関数を読み込む

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <div class="lifegame-container">
   <div class="lifegame-cells-container">
     <div class="lifegame-cells">
-      {#each grid as row, i}
+      {#each $lifegame.grid as row, i}
         {#each row as col, j}
-          <div class="cell" style="grid-row: {i + 1}" />
+          <div
+            class="cell"
+            class:alive={col.isAlive}
+            style="grid-row: {i + 1}"
+            on:click={(e) => lifegame.toggle(i, j)}
+          />
         {/each}
       {/each}
     </div>
@@ -11,16 +16,12 @@
 </div>
 
 <script>
+import { createLifeGame } from './lifegame'
+
 const row_size = 20
 const column_size = 20
 
-const grid = []
-for (let i = 0; i < row_size; i++) {
-  grid[i] = []
-  for (let j = 0; j < column_size; j++) {
-    grid[i][j] = { isAlive: false }
-  }
-}
+const lifegame = createLifeGame(row_size, column_size)
 </script>
 
 <style>
